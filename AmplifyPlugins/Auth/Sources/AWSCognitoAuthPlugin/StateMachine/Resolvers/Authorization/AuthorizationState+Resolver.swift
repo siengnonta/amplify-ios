eeae2d185e21bd659f73477a0cc6e8b0077dd5f8
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-06-24 02:58:40 -0400

    feat(Auth): Adding delete user state machine implementation

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/StateMachine/Resolvers/Authorization/AuthorizationState+Resolver.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/StateMachine/Resolvers/Authorization/AuthorizationState+Resolver.swift
@@ -58,6 +58,10 @@ extension AuthorizationState {
                         existingCredentials: credentials,
                         subState), actions: [action])
                 }
+                
+                if case .deleteUser = event.isDeleteUserEvent {
+                    return .init(newState: .deletingUser)
+                }
 
                 return .from(oldState)
 
@@ -131,6 +135,16 @@ extension AuthorizationState {
                 }
                 return .from(oldState)
 
+            case .deletingUser:
+                if case .userSignedOutAndDeleted = event.isDeleteUserEvent {
+                    return .init(newState: .configured)
+                }
+                if let signOutEvent = event.isSignOutEvent,
+                    case .signOutLocally = signOutEvent {
+                    return .init(newState: .waitingToStore(.noCredentials))
+                }
+                return .from(oldState)
+                
             case .error:
                 if case .fetchUnAuthSession = event.isAuthorizationEvent {
                     let action = InitializeFetchUnAuthSession()
